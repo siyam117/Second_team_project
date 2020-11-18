a15c6c4ca14e7abcc26653509d3eaa0bd521e23f
Kaiwei Guo <kaiwei.guo@student.manchester.ac.uk>
2020-11-18 18:24:08 +0000

    Release 1.36uom
    Updated AUTHORS, CHANGES and version number.

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-STENDHAL_VERSION="1.35uom"
+STENDHAL_VERSION="1.36uom"
 
 LOCALCLASSPATH=.:data/script/:data/conf/:stendhal-server-$STENDHAL_VERSION.jar:marauroa.jar:mysql-connector.jar:log4j.jar:commons-lang.jar:h2.jar
 
